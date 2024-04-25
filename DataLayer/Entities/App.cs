ce227561540439a67658877fc40cd5bca6ed013c
William Sl <william.sl@outlook.com>
2024-04-25 22:20:00 +0200

    Add app entity

---
+++ DataLayer/Entities/App.cs
@@ -0,0 +1,10 @@
+﻿namespace DataLayer.Entities
+{
+    public class App
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public User User { get; set; }
+        public bool HasAccess { get; set; }
+    }
+}
--- DataLayer/Entities/User.cs
+++ DataLayer/Entities/User.cs
@@ -8,8 +8,9 @@ namespace DataLayer.Entities
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-
         [Required]
         public string SaltHex { get; set; }
+
+        public ICollection<App> Apps { get; set; }
     }
 }
